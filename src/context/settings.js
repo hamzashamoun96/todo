6835ea4770a377ffe7481f0307aa59ab6ae1368d
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-05-18 21:18:44 +0300

    lab33

---
+++ src/context/settings.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+export const SettingsCon = React.createContext();
+function SettingsProv(props){
+    const [list , setList] = useState([]);
+    const [page , setPage] = useState(3);
+    const [sNum , setSnum] = useState(3);
+    
+    const state ={
+        list : list.sort((a,b)=>a.difficulty - b.difficulty),
+        setList,
+        page,
+        setPage,
+        sNum,
+        setSnum,
+        pages: list.slice(page-sNum,page),
+        num:gg()
+    }
+    function gg (){
+        let numArr = [];
+        for(let i = 0 ; i < Math.ceil(list.length/sNum); i++){
+            numArr.push( i + 1 )
+        }
+        return numArr;
+    }
+    return(
+        <SettingsCon.Provider value = {state}>
+            {props.children}
+        </SettingsCon.Provider>
+    );
+}
+export default SettingsProv;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import SettingsProv from './context/settings';
 function Main (){
   return(
-    <App/>
+    <SettingsProv>
+      <App/>
+    </SettingsProv>
   );
 }
 
